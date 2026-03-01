fa773fe20455f7d5e257066d7c09bdafa19c049a
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2026-03-01 05:31:27 +0530

    Update 1876-substrings-of-size-three-with-distinct-characters.java

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -16,3 +16,67 @@ class Solution {
         return count;
     }
 }
+
+/* Using Method:
+
+import java.util.*;
+
+public class window {
+
+	public static int code(String s,int count) {	
+		// TODO Auto-generated method stub
+		for(int i=0;i<s.length()-3;i++) {
+			char a =s.charAt(i);
+			char b = s.charAt(i+1);
+			char c = s.charAt(i+2);
+		
+			if(a!=b && b!=c && c!=a) {
+				count++;
+			
+		}
+	}
+		return count ;
+	}
+		
+		public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int count =0;
+		String s =scan.next();
+		System.out.println(code(s,count));
+		scan.close();
+	}
+
+} */
+
+/* Using Codeforces approach:
+    package slidingwindow;
+
+import java.util.*;
+
+public class window {
+
+		
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int count =0;
+		String s =scan.next();
+		for(int i=0;i<s.length()-3;i++) {
+			char a =s.charAt(i);
+			char b = s.charAt(i+1);
+			char c = s.charAt(i+2);
+			
+			if(a!=b && b!=c && c!=a) {
+				count++;
+				
+			}
+			
+			
+			
+		}
+		System.out.println(count);
+		scan.close();
+	}
+
+}
+*/
