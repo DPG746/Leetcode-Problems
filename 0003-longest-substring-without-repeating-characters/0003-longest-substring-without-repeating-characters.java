ab150ac26c2b92be036b98a874cd7411f16af5c4
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2026-03-17 04:22:48 +0530

    Time: 7 ms (37.31%), Space: 46.6 MB (26.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -24,36 +24,3 @@ class Solution {
         return maxLen;
     }
 }
-
-//Eclipse execution
-/* 
-import java.util.*;
-
-public class window {
-
-		
-		public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String s=scan.next();
-		Set<Character> set=new HashSet<>();
-		int l=0;
-		int max=0;
-		
-		for(int r=0;r<s.length();r++) {
-			while(set.contains(s.charAt(r))) {
-				set.remove(s.charAt(l));
-				l++;
-			}
-			set.add(s.charAt(r));
-			max=Math.max(max, r-l+1);
-			
-			
-		}
-		System.out.println(max);
-		scan.close();
-		
-		
-	}
-
-}
-*/
