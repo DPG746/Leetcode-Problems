65b30a9efaae354ec1e448ea872d96f346d4c30b
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2026-05-02 15:53:44 +0530

    Time: 2 ms (19.62%), Space: 46.1 MB (98.92%) - LeetHub

--- 3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.java
+++ 3912-valid-elements-in-an-array/3912-valid-elements-in-an-array.java
@@ -1,27 +1,30 @@
 class Solution {
     public List<Integer> findValidElements(int[] nums) {
-        int [] arr=new int [nums.length];
+        int[] arr = new int[nums.length];
+        List<Integer> ll = new LinkedList<>();
+        int i=0;
         int min=Integer.MIN_VALUE;
-        int i;
         for(i=0;i<nums.length;i++){
             if(min<nums[i]){
                 min=nums[i];
                 arr[i]=nums[i];
             }
         }
-        min=Integer.MIN_VALUE;
-        for(i=nums.length-1;i>=0;i--){
-            if(min<nums[i]){
-                min=nums[i];
-                arr[i]=nums[i];
+             min=Integer.MIN_VALUE;
+             for(i=nums.length-1;i>=0;i--){
+                 if(min<nums[i]){
+                     min=nums[i];
+                     arr[i]=nums[i];
+
+                 }
             }
-        }
-        List<Integer> l=new LinkedList<>();
-        for(i=0;i<arr.length;i++){
-            if(arr[i]!=0){
-                l.add(arr[i]);
-            }
-        }
-        return l;
+         for(i=0;i<nums.length;i++){
+             if(arr[i]!=0){
+                 ll.add(arr[i]);
+
+             }
+
+         }
+        return ll;
     }
 }
