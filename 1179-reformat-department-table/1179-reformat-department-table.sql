764bbf5a658408cc6e6428fe74d14fc20aa68c3e
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2026-05-26 07:50:22 +0530

    Time: 526 ms (55.44%), Space: 0B (100%) - LeetHub

--- 1179-reformat-department-table/1179-reformat-department-table.sql
+++ 1179-reformat-department-table/1179-reformat-department-table.sql
@@ -1,16 +1,17 @@
-# Write your MySQL query statement below
 select id,
-    SUM(case when month = "Jan" then revenue end) as Jan_Revenue,
-    SUM(case when month = "Feb" then revenue end) as Feb_Revenue,
-    SUM(case when month = "Mar" then revenue end) as Mar_Revenue,
-    SUM(case when month = "Apr" then revenue end) as Apr_Revenue,
-    SUM(case when month = "May" then revenue end) as May_Revenue,
-    SUM(case when month = "Jun" then revenue end) as Jun_Revenue,
-    SUM(case when month = "Jul" then revenue end) as Jul_Revenue,
-    SUM(case when month = "Aug" then revenue end) as Aug_Revenue,
-    SUM(case when month = "Sep" then revenue end) as Sep_Revenue,
-    SUM(case when month = "Oct" then revenue end) as Oct_Revenue,
-    SUM(case when month = "Nov" then revenue end) as Nov_Revenue,
-    SUM(case when month = "Dec" then revenue end) as Dec_Revenue
-from Department 
-group by id;
+MAX(if(month='Jan',revenue,NULL)) as JAN_Revenue,
+MAX(if(month='FEb',revenue,NULL)) as FEB_Revenue,
+MAX(if(month='Mar',revenue,NULL)) as MAR_Revenue,
+MAX(if(month='Apr',revenue,NULL)) as Apr_Revenue,
+MAX(if(month='May',revenue,NULL)) as May_Revenue,
+MAX(if(month='Jun',revenue,NULL)) as Jun_Revenue,
+MAX(if(month='Jul',revenue,NULL)) as Jul_Revenue,
+MAX(if(month='Aug',revenue,NULL)) as Aug_Revenue,
+MAX(if(month='Sep',revenue,NULL)) as Sep_Revenue,
+MAX(if(month='Oct',revenue,NULL)) as Oct_Revenue,
+MAX(if(month='Nov',revenue,NULL)) as Nov_Revenue,
+MAX(if(month='Dec',revenue,NULL)) as Dec_Revenue
+
+FROM Department
+group by id
+order by id;
