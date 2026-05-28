1931435214e41dc07556f506cfb590420214dac8
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2026-05-29 03:41:22 +0530

    Time: 0 ms (100%), Space: 47.1 MB (72.36%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,30 +1,33 @@
 class Solution {
-    public void dfs(int node,int[][] con,boolean[] visit){
-        visit[node]=true;
-        for(int i=0;i<con.length;i++){
-            if(con[node][i]==1 && !visit[i]){
-                dfs(i,con,visit);
+    public  void dfs(int[][] con,boolean[] isvisible,int i){
+        if(isvisible[i]==false){
+            isvisible[i]=true;
+            for(int j=0;j<con.length;j++){
+                if(con[i][j]==1 && !isvisible[j]){
+                    dfs(con,isvisible,j);
+
+                }
 
             }
+
 
         }
 
     }
-    
-        
     public int findCircleNum(int[][] con) {
-        
         int n=con.length;
-        int count=0;
-        boolean[] visit= new boolean[n];
+        boolean[] isvisible = new boolean[n];
+        int cnt=0;
         for(int i=0;i<n;i++){
-            if(!visit[i]){
-                count++;
-                dfs(i,con,visit);
+            if(isvisible[i]==false){
+                dfs(con,isvisible,i);
+                cnt++;
 
             }
 
         }
-        return count;
+        return cnt;
+
     }
+
 }
