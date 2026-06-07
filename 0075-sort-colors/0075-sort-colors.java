856c987cdbd0b26929c2563a583bbf4c4123c7a8
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2026-06-07 20:38:47 +0530

    Time: 3 ms (12.69%), Space: 43.1 MB (93.9%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,7 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n=nums.length;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             for(int j=0;j<n-i-1;j++){
                 if(nums[j]>nums[j+1]){
                     int temp=nums[j];
@@ -10,6 +10,7 @@ class Solution {
                 }
 
             }
+
         }
     }
 }
