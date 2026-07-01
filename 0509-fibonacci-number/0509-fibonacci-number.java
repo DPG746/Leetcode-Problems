a17afe50db32573bacbc47f7e7cdae3b311b5201
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2026-07-01 20:39:00 +0530

    Time: 0 ms (100%), Space: 41.9 MB (69.53%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int fib(int n) {
+       double phi = (1 + Math.sqrt(5)) / 2;
+        double psi = (1 - Math.sqrt(5)) / 2;
+
+        return (int) Math.round((Math.pow(phi, n) - Math.pow(psi, n)) / Math.sqrt(5));
+    }
+}
